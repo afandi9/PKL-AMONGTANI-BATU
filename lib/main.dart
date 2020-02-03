40e3550e8dadd04ff1cf916143609c0f06a61e12
pramudyaking <pramudyakingdom@gmail.com>
2020-02-03 13:14:07 +0700

    coba

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'dart:js';
 
 import 'package:flutter/material.dart';
 import 'login_page.dart';
+import 'home_page.dart';
 
 
 void main() => runApp(MyApp());
@@ -9,6 +9,8 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   final routes = <String, WidgetBuilder>{
     LoginPage.tag: (context) => LoginPage(),
+    HomePage.tag: (context) => HomePage(),
+
   };
   @override
   Widget build(BuildContext context) {
