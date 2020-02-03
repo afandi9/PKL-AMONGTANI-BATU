ca5b0dc7c272468258de50e0c5bf2e305dd6e971
pramudyaking <pramudyakingdom@gmail.com>
2020-02-03 15:42:10 +0700

    coba

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,7 +12,7 @@ class HomePage extends StatelessWidget {
         child: CircleAvatar(
           radius: 72.0,
           backgroundColor: Colors.transparent,
-          backgroundImage: AssetImage('assets/logo.jpeg'),
+          backgroundImage: AssetImage('assets/logo.png'),
         ),
       ),
     );
