dc060805ad1acc2cb6dfd9d5facfa29070ae1d51
CounterSnake <namikazeafandi9@gmail.com>
2020-02-04 09:03:16 +0700

    home

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,70 +1,98 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  static String tag = 'home-page';
+class HomePage extends StatefulWidget {
+  static String tag = 'BottomNavPage';
+  @override
+  _BottomNavPageState createState() => _BottomNavPageState();
+}
+
+class _BottomNavPageState extends State<HomePage>{
+  int _selectTabIndex = 0;
+
+  void _onNavBarTapped(int index){
+    setState(() {
+      _selectTabIndex = index;
+    });
+  }
 
   @override
-  Widget build(BuildContext context) {
-    final profil = Hero(
-      tag: 'hero',
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: CircleAvatar(
-          radius: 72.0,
-          backgroundColor: Colors.transparent,
-          backgroundImage: AssetImage('assets/logo.png'),
-        ),
-      ),
-    );
+  Widget build(BuildContext context){
+    final _listPage = <Widget>[
+      Text('Halaman Home'),
+      Text('Halaman Inbox'),
+      Text('Halaman Layanan'),
+    ];
 
-    final welcome = Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text(
-        'Selamat datang',
-        style: TextStyle(fontSize: 28.0, color: Colors.white),
-      ),
-    );
 
-    final paragraf1 = Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text(
-        'Halo, nama saya Akhmad Fuady Rifkiyanto, Sering dipanggil Rifki (di rumah) dan Fuad (di kampus).',
-        style: TextStyle(fontSize: 16.0, color: Colors.white),
+    final _bottomNavBarItems = <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        title: Text('Home'),
       ),
-    );
+      BottomNavigationBarItem(
+          icon: Icon(Icons.mail_outline),
+          title: Text('Inbox')
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.dvr),
+        title: Text('Layanan'),
+      ),
+    ];
 
-    final paragraf2 = Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text(
-        'Saat ini, saya adalah freelance di salah satu instansi di Kabupaten Batang.',
-        style: TextStyle(fontSize: 16.0, color: Colors.white),
-      ),
-    );
-
-    final paragraf3 = Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text(
-        'Pernah menempuh pendidikan di STMIK Widya Pratama Pekalongan mengambil program studi Teknik Informatika.',
-        style: TextStyle(fontSize: 16.0, color: Colors.white),
-      ),
-    );
-
-    final body = Container(
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.all(28.0),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Colors.blue,
-          Colors.lightBlueAccent,
-        ]),
-      ),
-      child: Column(
-        children: <Widget>[profil, welcome, paragraf1,paragraf2,paragraf3],
-      ),
+    final _bottomNavBar = BottomNavigationBar(
+      items: _bottomNavBarItems,
+      currentIndex: _selectTabIndex,
+      selectedItemColor: Colors.green,
+      unselectedItemColor: Colors.grey,
+      onTap: _onNavBarTapped,
     );
 
     return Scaffold(
-      body: body,
+      appBar: PreferredSize(
+        preferredSize: Size(null, 50),
+        child: Container(
+          decoration: BoxDecoration(
+              boxShadow: [BoxShadow(
+                  color: Colors.black12,
+                  spreadRadius: 5,
+                  blurRadius: 2
+              )]
+          ),
+          width: MediaQuery.of(context).size.width,
+          height: 80,
+          child: ClipRRect(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(15),
+                bottomRight: Radius.circular(15)
+            ),
+            child: Container(
+              color: Colors.white70,
+              child: Container(
+                margin: EdgeInsets.fromLTRB(10, 25, 10, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: Image.asset('assets/logo.png', fit: BoxFit.contain, height: 40,alignment: Alignment.centerLeft,),
+                    ),
+                    Expanded(
+                      child: Text("AMONG ABDI",style: TextStyle(fontSize: 15,color: Colors.green),textAlign: TextAlign.center,),
+                    ),
+                    Expanded(
+                      child: Icon(Icons.home, color: Colors.transparent),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: _listPage[_selectTabIndex],
+      ),
+      bottomNavigationBar: _bottomNavBar,
     );
   }
 }
